ddf6b6f64c8108ea301314dfdb7d63502f87a57a
jonask137 <73840082+jonask137@users.noreply.github.com>
2021-04-29 15:56:35 +0200

    Updating so it contains the create directory functions

--- Loading and storing the data.R
+++ Loading and storing the data.R
@@ -3,6 +3,20 @@ library(readr)
 library(tidyr)
 library(base)
 
+## Creating the folders
+
+base_dir <- "Data"
+dir.create(base_dir)
+train_dir <- file.path(base_dir,"train")
+validation_dir <- file.path(base_dir,"validation")
+dir.create(train_dir)
+dir.create(validation_dir)
+labels <- c("scab", "healthy", "frog_eye_leaf_spot", "rust", "complex")
+for (label in labels) {
+  dir.create(file.path(train_dir,label))
+  dir.create(file.path(validation_dir,label))
+}
+
 data <- read_csv("Data/plant-pathology-2021-fgvc8/train.csv")
 data <- as.data.frame(data)
 data_sep_rows <- separate_rows(data, labels, sep= " ")
