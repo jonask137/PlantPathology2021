a7f1265afdf6a761c6f0a971ef357b8df037e04e
Jonas Ringive Korsholm <Jonas.k345@gmail.com>
2021-04-30 11:16:01 +0200

    VGG16

--- VGG19.R
+++ VGG19.R
@@ -1,94 +1,60 @@
-
 # Libraries ----
 library(keras)
 library(readr)
 library(tidyr)
 library(base)
+library(doParallel)
 
 # Defining the model ----
-
 #Creating train and validation dir
-
 base_dir <- "Data"
-
 train_dir <- file.path(base_dir, "train")
 validation_dir <- file.path(base_dir, "validation")
-
 data <- read_csv("Data/plant-pathology-2021-fgvc8/train.csv")
 data <- as.data.frame(data)
 data_sep_rows <- separate_rows(data, labels, sep= " ")
 partion_size <- c(.75,.25)
 train_index <- sample(1:nrow(data_sep_rows), size = partion_size[1]*nrow(data_sep_rows))
-
 train <- data_sep_rows[train_index,]
 validation <- data_sep_rows[-train_index,]
 
-
 ## Setting tuning params ----
+batch_size <- 32
+target_size <- c(150,150)
 
-batch_size <- 16
-target_size <- c(300,200)
-
+## Making data generators -----
+train_generator <- flow_images_from_directory(train_dir, generator = image_data_generator(),
+                                              target_size = target_size, color_mode = "rgb",
+                                              class_mode = "categorical", batch_size = batch_size, shuffle = TRUE,
+                                              seed = 123)
+validation_generator <- flow_images_from_directory(validation_dir, generator = image_data_generator(),
+                                                   target_size = target_size, color_mode = "rgb", classes = NULL,
+                                                   class_mode = "categorical", batch_size = batch_size, shuffle = TRUE,
+                                                   seed = 123)
+train_samples = nrow(train)
+validation_samples = nrow(validation)
 
 ## Constructing the model ----
+#base_model <- application_inception_v3(weights = 'imagenet', include_top = FALSE)
+base_model <- application_vgg16(weights = 'imagenet', include_top = FALSE)
 
-conv_base <- application_vgg19(
-  weights = "imagenet",
-  include_top = FALSE, #Do we want the densely connected layers?
-  input_shape = c(300, 200, 3)
-)
+## add your custom layers
+predictions <- base_model$output %>% 
+  layer_global_average_pooling_2d(trainable = T) %>% 
+  layer_dense(64, trainable = T) %>%
+  layer_activation("relu", trainable = T) %>%
+  layer_dropout(0.4, trainable = T) %>%
+  layer_dense(5, trainable = T) %>%
+  layer_activation("softmax", trainable = T)
 
-conv_base
+# this is the model we will train
+model <- keras_model(inputs = base_model$input, outputs = predictions)
 
-model <- keras_model_sequential() %>% 
-  layer_conv_2d(filters = 64, kernel_size = c(3, 3), activation = "relu", 
-                input_shape = c(300, 200, 3)) %>% 
-  layer_max_pooling_2d(pool_size = c(2, 2)) %>% 
-  layer_conv_2d(filters = 128, kernel_size = c(3, 3), activation = "relu") %>% 
-  layer_max_pooling_2d(pool_size = c(2, 2)) %>% 
-  layer_conv_2d(filters = 256, kernel_size = c(3, 3), activation = "relu") %>% 
-  layer_max_pooling_2d(pool_size = c(2, 2)) %>% 
-  layer_conv_2d(filters = 256, kernel_size = c(3, 3), activation = "relu") %>% 
-  layer_max_pooling_2d(pool_size = c(2, 2)) %>% 
-  layer_flatten() %>% 
-  layer_dense(units = 512, activation = "relu") %>% 
-  layer_dense(units = 5, activation = "softmax") #We have 5 different categories
+### Freeze the layer weights in VGG16
+for (layer in base_model$layers)
+  layer$trainable <- FALSE
 
-model %>% compile(
-  loss = "categorical_crossentropy", #we have binary classification
-  optimizer = optimizer_rmsprop(lr = 1e-4), #we know its tunable, but we start at some place"
-  metrics = c("acc") #we have balanced data, so acc should be ok!
-)
-
-
-
-## Data generators ----
-
-# All images will be rescaled by 1/255
-train_datagen <- image_data_generator(rescale = 1/255)
-validation_datagen <- image_data_generator(rescale = 1/255)
-
-
-train_generator <- flow_images_from_directory(
-  # This is the target directory
-  train_dir,
-  # This is the data generator
-  train_datagen,
-  # All images will be resized to 150x150
-  target_size = target_size,
-  batch_size = batch_size,
-  # Since we use binary_crossentropy loss, we need binary labels
-  class_mode = "categorical"
-)
-
-
-validation_generator <- flow_images_from_directory(
-  validation_dir,
-  validation_datagen,
-  target_size = target_size,
-  batch_size = batch_size,
-  class_mode = "categorical"
-)
+## Set call backs ----
 
 ## Making Call back list ----
 
@@ -99,30 +65,27 @@ callbacks_list <- list(
   ),
   callback_reduce_lr_on_plateau(monitor = "val_loss",
                                 factor = 0.1,
-                                patience = 10
+                                patience = 4
   )
 )
 
-
-
-# Compiling the model ----
-
+################### Section 5 #########################
 model %>% compile(
-  loss = "categorical_crossentropy", #we have binary classification
-  optimizer = optimizer_rmsprop(lr = 1e-4), #we know its tunable, but we start at some place"
-  metrics = c("acc"), #we have balanced data, so acc should be ok!
+  loss = "categorical_crossentropy",
+  optimizer = optimizer_rmsprop(lr = 0.003, decay = 1e-6),  ## play with the learning rate
+  metrics = "accuracy"
 )
-
-
-# Running the model ----
-
-train_steps_per_epoch <- round(nrow(train)/batch_size)
-validation_steps_per_epoch <- round(nrow(validation)/batch_size)
 
 history <- model %>% fit_generator(
   train_generator,
-  steps_per_epoch = train_steps_per_epoch,
-  epochs = 30,
+  steps_per_epoch = as.integer(train_samples/batch_size)/2, 
+  epochs = 20, 
   validation_data = validation_generator,
-  validation_steps = validation_steps_per_epoch
+  validation_steps = as.integer(validation_samples/batch_size)/2,
+  workers = detectCores()-1,
+  callbacks = callbacks_list
 )
+
+save_model_weights_hdf5(model, "Saved Objects/vgg19_model.h5")
+save_model_weights_hdf5(history, "Saved Objects/vgg19_history.h5")
+saveRDS(history,"Saved objects/vgg19_history.rds")
